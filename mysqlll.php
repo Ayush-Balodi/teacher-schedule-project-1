fce3156281393ad08f1304158bbba2adc7b1470c
Ayush Balodi <ayushbalodidun@gmail.com>
2022-05-18 17:11:30 +0530

    Added php

---
+++ mysqlll.php
@@ -0,0 +1,16 @@
+<?php
+    //comection
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "wow";
+
+    $conn = mysqli_connect($servername , $username , $password , $database);
+
+    if( !$conn ){
+        die("Sorry we failed to connect".mysqli_connect_error());
+    }
+    else{
+        echo "Connected successfully.";
+    }
+?>
